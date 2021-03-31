fd7a2e965bdf9599a8e3a5d19716ae33f1697e4b
SMARCOCCIA <zertyx2008@yahoo.fr>
2021-03-31 21:01:36 +0200

    Suppresion de codes inutiles

--- src/main/java/fr/clelia/jade2/dao/OrigineDao.java
+++ src/main/java/fr/clelia/jade2/dao/OrigineDao.java
@@ -1,12 +1,9 @@
 package fr.clelia.jade2.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.clelia.jade2.business.Origine;
 
 public interface OrigineDao extends JpaRepository<Origine, Integer> {
 	
-	List<Origine> recupererOrigines();
 }
--- src/main/java/fr/clelia/jade2/dao/PersonneDao.java
+++ src/main/java/fr/clelia/jade2/dao/PersonneDao.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import fr.clelia.jade2.business.Personne;
 
 
-
 public interface PersonneDao extends JpaRepository<Personne, Integer> {
 	
 	Personne findByEmailAndMotDePasse(String email, String motDePasseCompteWeb);
