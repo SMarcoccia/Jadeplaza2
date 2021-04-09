7b15778a94fa728217ee6e0daceca6f533248c11
SMARCOCCIA <zertyx2008@yahoo.fr>
2021-04-09 14:13:02 +0200

    Ajout de la méthode findAllById avec en paramètre l'id,  plus un type Pageable

--- src/main/java/fr/clelia/jade2/dao/AppelDao.java
+++ src/main/java/fr/clelia/jade2/dao/AppelDao.java
@@ -1,17 +1,22 @@
 package fr.clelia.jade2.dao;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import fr.clelia.jade2.business.Appel;
 
-public interface AppelDao extends JpaRepository<Appel, Integer> {
 
-	@Query(value="SELECT * FROM phone_call ph_c WHERE ph_c.PHONECALLID = :id", nativeQuery=true)
-	List<Appel> findAppels(@Param("id") Integer id);
+public interface AppelDao extends JpaRepository<Appel, Integer> {
+	
+	final String PHONECALLID = "PHONECALLID";
 		
-	//List<Appel> findAppelsFiltrer();
+	@Query(value="SELECT * FROM phone_call ph_c WHERE ph_c." + PHONECALLID + " = :id", nativeQuery=true)
+	Appel findAppel(@Param("id") Integer id);
+
+	@Query(value="SELECT * FROM phone_call ph WHERE ph.followedby = :id", nativeQuery=true)
+	Page<Appel> findAllById(@Param("id") int id, Pageable pageable);
 }
+
