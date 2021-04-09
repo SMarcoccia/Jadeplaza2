8d016b87eea57eae481f5a04bced9707abc333cf
SMARCOCCIA <zertyx2008@yahoo.fr>
2021-04-09 14:18:43 +0200

    Ajout des méthodes recupererMesAppels, ajouterAppel, modification du nom de la méthode recupererAppels(Integer id); par recupererAppelsFiltrer, modification de la méthode recupererAppelsFiltrer(Map<String, String> map, Pageable pageable); en recupererAppelsFiltrer(Map<String, String> map);

--- src/main/java/fr/clelia/jade2/service/AppelService.java
+++ src/main/java/fr/clelia/jade2/service/AppelService.java
@@ -1,6 +1,7 @@
 package fr.clelia.jade2.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,10 +9,15 @@ import org.springframework.data.domain.Pageable;
 import fr.clelia.jade2.business.Appel;
 
 public interface AppelService {
+	
+	Page<Appel> recupererMesAppels(int id, Pageable pageable);
 
 	List<Appel> recupererAppels();
-	List<Appel> recupererAppels(Integer id);
 	Page<Appel> recupererAppels(Pageable pageable);
-
-	//Page<Appel> recupererAppelsFiltrer(Map<String, String> map, Pageable pageable);
+	
+	Appel recupererAppelParId(Integer id);
+	
+	Appel ajouterAppel(Appel appel);
+	
+	List<Appel> recupererAppelsFiltrer(Map<String, String> map);
 }
