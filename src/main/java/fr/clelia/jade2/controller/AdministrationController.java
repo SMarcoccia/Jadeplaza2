aec61eb95543d7d4f2780d06ca4233cd5861223b
SMARCOCCIA <zertyx2008@yahoo.fr>
2021-04-27 16:54:39 +0200

    Création des méthodes pour afficher la liste des comptes

--- src/main/java/fr/clelia/jade2/controller/AdministrationController.java
+++ src/main/java/fr/clelia/jade2/controller/AdministrationController.java
@@ -1,14 +1,23 @@
 package fr.clelia.jade2.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.clelia.jade2.business.Appel;
+import fr.clelia.jade2.business.Personne;
 import fr.clelia.jade2.service.AgenceService;
 import fr.clelia.jade2.service.AnnonceService;
 import fr.clelia.jade2.service.AppelService;
@@ -45,6 +54,54 @@ public class AdministrationController {
 		this.origineService = origineService;
 	}
 
+	
+	
+	@PostMapping({"/ajouter-un-compte", "/modifier-un-compte"})
+	public ModelAndView comptePost(
+		@Valid @ModelAttribute("personne") Personne personne,
+		@RequestParam(required=false, value="idpersonne") String idpersonne,
+		BindingResult result
+	) {
+		Personne personneValue = (Personne) httpSession.getAttribute("personne");
+		if(personneValue != null && personneValue.getEmail().equals("fxcote@clelia.fr")) {
+			if(result.hasErrors()) {
+	    		ModelAndView mav = new ModelAndView();
+	    		mav.addObject("idpersonne", idpersonne);
+	    		mav.addObject("personne", personne);
+	    		mav.setViewName("compte");
+	    		return mav;
+	    	}else {
+	    		personne.setDateHeureCreation(new Date());
+	    		personneService.ajouterPersonne(personne);
+	    		ModelAndView mav = new ModelAndView("redirect:liste-des-comptes");
+	    		return mav;
+	    	}
+		}else {
+			return new ModelAndView("redirect:index");
+		}
+	}
+
+	@GetMapping({"/ajouter-un-compte", "/modifier-un-compte"})
+	public ModelAndView compteGet( 
+		@ModelAttribute("personne") Personne personne, 
+		@RequestParam(required=false, value="idPersonne") String idPersonne
+	) {
+		Personne personneValue = (Personne) httpSession.getAttribute("personne");
+		if(personneValue != null && personneValue.getEmail().equals("fxcote@clelia.fr")) {
+			ModelAndView mav = new ModelAndView();
+			
+			if(idPersonne != null) {
+				mav.addObject("personne", personneService.recupererPersonneParId(Integer.parseInt(idPersonne)));
+			}
+
+			mav.addObject("idPersonne", idPersonne);
+			mav.setViewName("compte");
+			return mav;
+		}else {
+			return new ModelAndView("redirect:index");
+		}
+	}
+	
 	@GetMapping({"/liste-des-comptes"})
 	public ModelAndView listeDesComptesGet(
 		@PageableDefault(
