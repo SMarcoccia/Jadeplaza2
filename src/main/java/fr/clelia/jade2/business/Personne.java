3ffc5d7ad73f41272a864035dd489175f1d025b2
SMARCOCCIA <zertyx2008@yahoo.fr>
2021-04-27 16:52:38 +0200

    RAS

--- src/main/java/fr/clelia/jade2/business/Personne.java
+++ src/main/java/fr/clelia/jade2/business/Personne.java
@@ -1,13 +1,23 @@
 package fr.clelia.jade2.business;
 
 import java.io.Serializable;
-import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import java.util.Date;
-import java.util.List;
+import org.springframework.format.annotation.DateTimeFormat;
 
 
 /**
@@ -54,7 +64,7 @@ public class Personne implements Serializable {
 	//@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Column(name="WEBLASTVISIT")
 	private Date dateHeureDerniereVisite;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	//@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Column(name="WEBACCOUNTCREATIONDATE")
